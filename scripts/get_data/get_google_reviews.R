b5d1096f3cc1e322832663b883ebda63e19fa306
Johan Rosa <johan.rosaperez@gmail.com>
2025-08-09 18:50:37 -0400

    feat: update google reviews as of 2025-08-09

--- scripts/get_data/get_google_reviews.R
+++ scripts/get_data/get_google_reviews.R
@@ -33,51 +33,32 @@ place_info_from_google <- function(query) {
     cbind(place_location(raw_result))
 }
 
-# places_from_google |> saveRDS("data/google/places_from_google.rds")
-
 google_revies_raw <- places_from_google$place_id |>
-  map(google_place_details, .progress = TRUE) |>
-  setNames(places_from_google$google_place_id)
-
+  set_names() |> 
+  map(google_place_details, .progress = TRUE)
 
 get_place_reviews <- function(place_details) {
   place_details$result$reviews
 }
 
 google_reviews <- map(google_revies_raw, get_place_reviews) |>
-  list_rbind(names_to = "google_place_id") |>
-  mutate(
-    time = as.numeric(time), 
-    date = as.POSIXct(time, origin = "1970-01-01", tz = "UTC") |> as.Date(),
-    month = floor_date(date, "month"),
-    quarter = floor_date(date, "quarter")
-  ) |>
-  as_tibble() |>
-  select(google_place_id, date, month, quarter, text, author_name, review_rating = rating)
-
-saveRDS(google_reviews, "data/google/reviews_from_google.rds")
-
-
-# Reviews form other places -----------------------------------------------
-other_places <- readRDS("data/google/other_places_from_google.rds") |>
-  as_tibble()
-
-other_places_revies_raw <- other_places$place_id |>
-  purrr::set_names() |> 
-  purrr::map(googleway::google_place_details, .progress = TRUE)
-
-other_places_reviews <- map(other_places_revies_raw, get_place_reviews) |>
   list_rbind(names_to = "place_id") |>
-  as_tibble() |>
   mutate(
     time = as.numeric(time), 
     date = as.POSIXct(time, origin = "1970-01-01", tz = "UTC") |> as.Date(),
     month = floor_date(date, "month"),
     quarter = floor_date(date, "quarter")
   ) |>
+  as_tibble() |>
   select(place_id, date, month, quarter, text, author_name, rating)
 
-other_places_reviews |>
-  distinct() |>
-  saveRDS("data/google/reviews_from_other_places.rds")
 
+
+if (file.exists("data/google/reviews.rds")) {
+  existing_reviews <- readRDS("data/google/reviews.rds")
+  google_reviews <- google_reviews |>
+    bind_rows(existing_reviews) |>
+    distinct()
+}
+
+saveRDS(google_reviews, "data/google/reviews.rds")
