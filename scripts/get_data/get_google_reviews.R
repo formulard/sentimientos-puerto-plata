8b5e4ae42e436684806ced703e0a5a7ecfda82a2
Johan Rosa <johan.rosaperez@gmail.com>
2025-08-09 18:36:22 -0400

    remove search of places from google using tripadvisor as reference

--- scripts/get_data/get_google_reviews.R
+++ scripts/get_data/get_google_reviews.R
@@ -12,48 +12,30 @@ library(lubridate)
 api_key <- Sys.getenv("GOOGLE_API_KEY")
 set_key(api_key)
 
-# Import data -------------------------------------------------------------
-places <- readRDS("data/tripadvisors/places.rds") |> as_tibble()
+places_from_google <- readRDS("data/google/places.rds") |> as_tibble()
+
+# Approach when we just had places from tripadvisor -----------------------
+
+#' when there weren't any places from google, I took the place name from 
+#' tripadvisor to query similar names in google places. This is not needed anymore
+#' there is a commit with the former logic.
 
 place_info_from_google <- function(query) {
   raw_result <- google_places(query, page_token = NULL)
-  
+
   if (raw_result$status == "ZERO_RESULTS") return(data.frame())
-  
-  raw_result$results |> 
-    as_tibble() |> 
+
+  raw_result$results |>
+    as_tibble() |>
     select(
       any_of(c("place_id","name", "rating", "types", "user_rating_total"))
     ) |>
     cbind(place_location(raw_result))
 }
 
-places_with_google_info <- places |>
-  rename(tripadvisor_id = place_id, tripadvisor_name = name) |>
-  mutate(
-    google_place_info = purrr::map(
-      paste(tripadvisor_name, destination, sep = ","),
-      place_info_from_google,
-      .progress = TRUE
-    )
-  )
+# places_from_google |> saveRDS("data/google/places_from_google.rds")
 
-places_from_google <- places_with_google_info |>
-  rename(tripadvisor_rating = rating) |> 
-  unnest(google_place_info) |>
-  rename(
-    google_rating = rating,
-    google_name = name,
-    google_place_id = place_id
-  ) |> 
-  group_by(google_place_id) |>
-  slice(1) |>
-  ungroup()
-
-places_from_google |> saveRDS("data/google/places_from_google.rds")
-
-
-google_revies_raw <- places_from_google$google_place_id |>
+google_revies_raw <- places_from_google$place_id |>
   map(google_place_details, .progress = TRUE) |>
   setNames(places_from_google$google_place_id)
 
