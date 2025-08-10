3fa0ec63285fc237f1fd757fdd71e566cdc02036
Johan Rosa <johan.rosaperez@gmail.com>
2025-08-10 10:49:30 -0400

    feat: clean and classify sotial network reviews

--- scripts/classify_reviews_with_llm.R
+++ scripts/classify_reviews_with_llm.R
@@ -39,3 +39,46 @@ new_reviews |>
   filter(!is.na(category)) |>
   bind_rows(existing_reviews) |>
   saveRDS("data/classified_reviews.rds")
+
+
+# Outsorced reviews -------------------------------------------------------
+
+outsourced_reviews <- readxl::read_excel("data/redes_sociales/reviews-redes-sociales.xlsx") |>
+  as_tibble() |> 
+  janitor::clean_names() |>
+  tidyr::fill(actividad, entidad, red_social) |>
+  dplyr::filter(!is.na(comentarios), !is.na(fecha)) |> 
+  dplyr::mutate(
+    date = janitor::excel_numeric_to_date(as.numeric(fecha)),
+    quarter = lubridate::floor_date(date, "quarter"),
+    month   = lubridate::floor_date(date, "month")
+  ) |> 
+  select(
+    source = red_social, 
+    actividad,  
+    place = entidad, 
+    date,
+    quarter,
+    month,
+    text = comentarios
+  ) |> 
+  mutate(
+    review_id = map_chr(paste(date, actividad, place, text), \(x) digest::digest(x))
+  ) |>
+  relocate(review_id)
+
+
+classified_out_rv <- outsourced_reviews |>
+  distinct() |> 
+  select(review_id, text) |>
+  filter(text != "") |>
+  classify_reviews_in_batch(
+    cache_path = "data/cache/outsourced_in_progress.rds",
+    batch_size = 10, 
+    model = "gpt-4o-mini"
+  ) |> 
+  as_tibble()
+  
+outsourced_reviews |>
+  left_join(classified_out_rv) |>
+  saveRDS("data/classified_sotial_network_reviews.rds")
